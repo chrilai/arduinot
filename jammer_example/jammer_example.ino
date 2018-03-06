61020208beca71a1e924d8026cf01ae9bf9b7ed3
chrilai <chrilai@gmail.com>
2018-03-06 02:23:08 -0700

    Add examples of fun stuff

---
+++ jammer_example/jammer_example.ino
@@ -0,0 +1,4 @@
+void setup(){ }
+void loop(){
+  tone(8, 15000); // generate square wave
+}
---
+++ usb_script_example/usb_script_example.ino
@@ -0,0 +1,36 @@
+#include "Keyboard.h"
+
+void typeKey(int key)
+{
+  Keyboard.press(key);
+  delay(50);
+  Keyboard.release(key);
+}
+
+void setup()
+{
+
+  Keyboard.begin();
+
+  delay(500);
+
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+
+  delay(200);
+
+  Keyboard.print("notepad");
+
+  typeKey(KEY_RETURN);
+
+  delay(200);
+
+  Keyboard.print("Hello, World!");
+
+  typeKey(KEY_RETURN);
+
+  Keyboard.end();
+}
+
+void loop() {}
